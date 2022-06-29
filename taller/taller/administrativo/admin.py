f7a3a51a4d80154639f82eae6f0c62cfb8d1a0b8
Anthonny222 <Anthonny222>
2022-06-28 19:53:43 -0500

    tablas en django

---
+++ taller/taller/administrativo/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from administrativo.models import Edificio, Departamento
+
+class EdificioAdmin(admin.ModelAdmin):
+
+    list_display = ('nombre', 'direccion', 'ciudad', 'tipo')
+    search_fields = ('nombre', 'direccion', 'ciudad', 'tipo')
+
+
+admin.site.register(Edificio, EdificioAdmin)
+
+
+class DepartamentoAdmin(admin.ModelAdmin):
+
+    list_display = ('propietario', 'costoDep', 'numCuartos', 'edificio')
+    search_fields = ('propietario', 'costoDep', 'numCuartos', 'edificio')
+
+    raw_id_fields = ('edificio',)
+
+admin.site.register(Departamento, DepartamentoAdmin)
